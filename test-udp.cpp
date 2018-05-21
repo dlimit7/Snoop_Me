98edbcb1561021386d3f7513469449bb1672a0c6
dlimit7 <daniel.li9721@gmail.com>
2018-05-21 19:55:12 +1000

    endian conversion done

--- test-udp.cpp
+++ test-udp.cpp
@@ -16,11 +16,8 @@ using namespace std;
 int main (void) {
     int client;
     int portNo = 8117;
-    char host[1024] = "skunksmail.ee.unsw.edu.au";
-    //string buffer;
-    int buffer[1];
-    buffer[0] = 1; // S value
-    unsigned int buffertosend[1];
+    //char host[1024] = "skunksmail.ee.unsw.edu.au";
+    unsigned int S[1] = {htonl(10)}; // S value
     char response[90];
 
    // printf("[*] Creating a socket...\n");
@@ -40,51 +37,47 @@ int main (void) {
     client_addr.sin_addr.s_addr = htonl(INADDR_ANY); // takes local host address... need to set -multi_snooper flag
     client_addr.sin_port = htons(8001);
 
-//    printf("[*] Binding client socket...");
-//    if (bind(client, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0) {
-//        printf("    => Error in binding socket");
-//        exit(1);
-//    }
-
-   // printf("[*] Validating IP Address...\n");
+    printf("[*] Validating IP Address...\n");
     if (inet_pton(AF_INET, "149.171.36.173", &serv_addr.sin_addr) <= 0) {
         printf("    => Invalid Address\n");
         exit(1);
     }
     int i = 0;
-    unsigned int S = 1;
-//    cout << *buffer << endl;
-    buffertosend[0] = htonl(buffer[0]);
-    //buffertosend[0] = buffer[0];
-//    cout << *buffertosend << endl;
     while (i < 30) {
-        //printf("[*] Attempting to send message via UDP\n");
-        //if (sendto(client, buffer.c_str(), buffer.length(), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        if (sendto(client, buffertosend,sizeof(buffertosend), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        if (sendto(client, S,sizeof(S), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
             printf("    => Error in sending the message\n");
             exit(1);
         }
-     //   printf("[*] Awaiting for response from the server...\n");
+        //printf("[*] Awaiting for response from the server...\n");
         socklen_t addrlen = sizeof(serv_addr);
         if (recvfrom(client, response, sizeof(response), 0, (struct sockaddr *)&serv_addr, &addrlen) < 0) {
             printf("    => Error in receiving the message\n");
             exit(1);
         }
-       // printf("[*] Snooped message\n");
-        //cout << response << endl;
         // Response has both the 8 byte packet identifier and the message.
-        // convert 8 byte big endian to little endian
+        // Convert 8 byte big endian to little endian
+        unsigned int lo = *(unsigned int *)response;
+        unsigned int  hi =  *(unsigned int*)(response+8);
         unsigned long long int identifier = *(unsigned long long int*)response;
-        printf("# 0x%llx:   ", identifier);
+        //printf("# 0x%llx:   \n", identifier);
+        lo = (unsigned int) (identifier);
+        lo = ntohl(lo);
+        identifier = identifier >> 32;
+        hi = (unsigned int) (identifier);
+        hi = ntohl(hi);
+        identifier = 0;
+        identifier |= lo;
+        identifier = identifier << 32;
+        identifier |= hi;
+        printf("Packet Identifier # 0x%llx:   ", identifier);
+
+        // The message
         char *msg = (char*)(response+8);
         cout << "\"" <<msg << "\"" << endl;
-        memset(response, 0, sizeof(response)+1);
-        memset(msg, 0, sizeof(msg)+1);
+        memset(response, 0, 20);
+        memset(msg, 0, 20);
         i++;
     }
-    //printf("[*] Awaiting for response from the server...\n");
-    //socklen_t addrlen = sizeof(serv_addr);
-    //recvfrom(client, response, sizeof(response), 0, (struct sockaddr *)&serv_addr, &addrlen);
     close(client);
     return 0;
 }
