2368c43bebd54b322d0d082d182e0a1db290636e
dlimit7 <daniel.li9721@gmail.com>
2018-05-25 21:16:43 +1000

    working draft

--- test-udp.cpp
+++ test-udp.cpp
@@ -61,7 +61,6 @@ void *snooper(void* serv_info) {
     int client_fd = server_info->get_socket();
     struct sockaddr_in serv_addr = server_info->get_sockstruct();
     while (1) {
-
         i%2 ? (S[0]--):(S[0]++); i++;
         unsigned int buffer[1] = {htonl(S[0])}; 
         if (sendto(client_fd, buffer,sizeof(buffer), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
@@ -78,13 +77,21 @@ void receive(ServerInfo *server_info) {
     struct sockaddr_in serv_addr = server_info->get_sockstruct();
     struct timeval timeout;
     char response[30];
+    //char *answer = NULL;
     int ret;
     char * msg;
     map<string, unsigned long long int>msgtoid;
+    map<unsigned int, string>msg_map;
     unsigned int num_packets = 0;
     unsigned int diff_array[10];
     unsigned int diff;
     int i = 0;
+    int num_packets_found = 0;
+    char ** message= NULL;
+    unsigned int offset;
+    unsigned int index;
+    int count = 0;
+    int offsetFound = -1;
     while (1) {
         //printf("[*] Awaiting for response from the server...\n");
         FD_ZERO(&readfds);
@@ -103,6 +110,7 @@ void receive(ServerInfo *server_info) {
             exit(1);
         }
         ///cout << "response " << response << endl;
+        mtx.unlock();
         warp(&response[0]);
         
         /**** 
@@ -112,57 +120,94 @@ void receive(ServerInfo *server_info) {
          * ***/
         msg = response+8;
         char first_char = msg[0];
+        int k;
         //string msg;
         //msg.assign(message, 30);
         //cout << "message is \"" << msg << "\"" << endl; 
-
         unsigned long long int id = *(unsigned long long int *)response;
-        if (msgtoid.count(msg) < 1) {
-            msgtoid[msg] = id;
-            num_packets++;
-        }   
-        if (first_char == 0x4) {
-            //printf("id 0x%llx - msgtoid[msg] 0x%llx\n\n\n\n\n", id, msgtoid[msg]);
-            if ((diff = (unsigned int)(id - msgtoid[msg])) > 0) {
-                if (diff == 0) exit(1);
-                //while(1);
-                // update id and record diff
+        if (!num_packets_found) { 
+            if (msgtoid.count(msg) < 1) {
                 msgtoid[msg] = id;
-                diff_array[i++] = diff;
-            }
-        }
-        if (i==10) {
-            //printf("num_packs = %d\n", num_packets);
-            //printf("\n\n\n\n\n\n");
-            i = 0;
-            // find lowest common divisor in diff_array
-            unsigned int min = 0xfff;
-            cout << min << endl;
-            unsigned int j = 0;
-            for (j = 0; j < 10; j++) {
-                if (diff_array[j] < min) {
-                    min = diff_array[j];
-                    //printf("min is %d j is %d diff[j]  is %d\n\n\n\n\n", min, j, diff_array[j]);   
+                num_packets++;
+            }   
+            if (first_char == 0x4) {
+                //printf("id 0x%llx - msgtoid[msg] 0x%llx\n\n\n\n\n", id, msgtoid[msg]);
+                if ((diff = (unsigned int)(id - msgtoid[msg])) > 0) {
+                    if (diff == 0) exit(1);
+                    //while(1);
+                    // update id and record diff
+                    msgtoid[msg] = id;
+                    diff_array[i++] = diff;
                 }
             }
-            int k;
-            j = 2;
-            while (j <= min) {
-               if (j >= num_packets) {
-                   for (k = 0; k < 10; k++) {
-                       if (diff_array[k] % j) break;
-                   }
-               }
-               if (k == 10) break;
-               j++;
+            if (i == 10) {
+                //printf("num_packs = %d\n", num_packets);
+                //printf("\n\n\n\n\n\n");
+                i = 0;
+                // find lowest common divisor in diff_array
+                unsigned int min = 0xfff;
+                cout << min << endl;
+                unsigned int j = 0;
+                for (j = 0; j < 10; j++) {
+                    if (diff_array[j] < min) {
+                        min = diff_array[j];
+                        printf("min is %d j is %d diff[j]  is %d\n\n\n\n\n", min, j, diff_array[j]);   
+                    }
+                }
+                j = 2;
+                while (j <= min) {
+                    if (j >= num_packets) {
+                        for (k = 0; k < 10; k++) {
+                            if (diff_array[k] % j) break;
+                        }
+                    }
+                    if (k == 10) break;
+                    j++;    
+                }
+                printf("number of packets = %d\n", j);
+                num_packets_found = j;
+                //while(1);
+                // malloc an array of num_packets amount of strings of size 20
+                message = (char**) malloc(num_packets_found * sizeof(char*));
+                //answer = (char*)malloc(num_packets_found*20);
+                for (k = 0; k < num_packets_found; k++) {
+                    message[k] = (char*)malloc(20);
+                }
+           }
+        } else {
+            /*
+             *
+             *  Packet reconstruction
+             *
+             */
+            if (first_char == 0x4 && offsetFound<0) {
+                count++;
+                offsetFound = 1;
+                index = id % num_packets_found;
+                printf("index here is %d\n", index);
+                offset = num_packets_found - index -1;
+                index = (index+offset)%num_packets_found;
+                msg_map[index] = message[index];
+                strcpy(message[index], msg); ;
+                printf("Message packet is \"%s\" at index %d\n", message[index], index);
+            } else if (offsetFound) {
+                index = id % num_packets_found;
+                index = (index+offset)%num_packets_found;
+                if (msg_map.count(index) == 0) {
+                    strcpy(message[index], msg);
+                    printf("Message packet is \"%s\" at index %d\n", message[index], index);
+                }
+                count++;
             }
-            printf("number of packets = %d\n", j);
-            while(1);
-
+            if ( count == num_packets_found ) {
+                while(1);
+            }
+            
         }
         
+
+
         memset(response, 0, 30);
-        mtx.unlock();
     }
 }
 
@@ -210,7 +255,7 @@ int main (int argc, char*argv[]) {
     client_addr.sin_addr.s_addr = htonl(INADDR_ANY); // takes local host address... need to set -multi_snooper flag
     client_addr.sin_port = htons(8001);
 */
-
+    
 
     close(client_fd);
 
